586b69835dc0d981b24fbf3cf94350a2b3c39203
firmament1005 <daviesrueben@gmail.com>
2024-05-24 09:10:12 -0700

    Created DashboardHeader file and Dashboard Nav bar

---
+++ src/components/Layout/DashboardHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const DashboardHeader: React.FC = () => {
+    return (
+        <h1>Dashboard Header</h1>
+    )
+}
+
+export default DashboardHeader;
---
+++ src/components/Layout/DashboardNav.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const DashboardNav: React.FC = () => {
+    return (
+        <h1>Dashboard Nav Bar</h1>
+    )
+}
+
+export default DashboardNav;
