48c634c45c083f5584dffd84e4966aef2cc502ea
BlueSky428 <bluesky.engineer428@gmail.com>
2024-05-26 22:22:09 -0700

    Upgraded some features

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,44 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '25%': { opacity: '0.25' },
+          '50%': { opacity: '0.5' },
+          '75%': { opacity: '0.75' },
+          '100%': { opacity: '1' },
+        },
+        'fade-out': {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+        'slide-in': {
+          '0%': { 
+            transform: 'translateY(-100%)',
+            opacity: '0'
+          },
+          '25%': { opacity: '0.25' },
+          '50%': { opacity: '0.5' },
+          '75%': { opacity: '0.75' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        'slide-out': {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { 
+            transform: 'translateY(-100%)',
+            opacity: '1'
+          },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 1s ease-out',
+        'fade-out': 'fade-out 1s ease-out',
+        'slide-in': 'slide-in .8s ease-out',
+        'slide-out': 'slide-out .8s ease-out',
+      },
+    },
   },
   plugins: [],
 }
-
