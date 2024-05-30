4eeb02c29cd41288ba812e4c6dfd6bc785746b47
firmament1005 <daviesrueben@gmail.com>
2024-05-30 01:09:10 -0700

    Added some features in chat page

--- src/pages/Chat/Message/Message.tsx
+++ src/pages/Chat/Message/Message.tsx
@@ -2,21 +2,46 @@ import React, { useState } from "react";
 import Header from "../../../components/Layout/Header";
 import MessageBody from "./MessageBody";
 import Footer from "../../../components/Layout/Footer";
+import axios from "axios";
 
 const Message: React.FC = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [newMessage, setNewMessage] = useState([]);
+
+    const handleSendQuestion = async (text = '', files: File[]) => {
+        if (files.length > 0) {
+            console.log(files[0].name);
+        }
+        try {
+            const config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: 'http://localhost:8080/api/chatgptwithchat',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: { "message": text }
+            };
+            const response = await axios.request(config);
+            const chatHistoryStored = localStorage.getItem("chatHistoryStore");
+            const chatHistory = chatHistoryStored ? JSON.parse(chatHistoryStored) : [];
+            chatHistory.push({ "question": text, "answer": response.data.msg });
+            localStorage.setItem("chatHistoryStore", JSON.stringify(chatHistory));
+            setNewMessage(chatHistory);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 
     return (
         <div className="w-full flex item-center flex-col">
             <div className={`flex-1 ${sidebarOpen ? 'ml-64 fxied' : ''}`}>
                 <Header />
             </div>
-            <MessageBody />
-            <Footer />
+            <MessageBody messages={newMessage} />
+            <Footer sendQuestions={handleSendQuestion} />
         </div>
     )
 }
 
 export default Message;
-
-// onSidebarOpen={() => setSidebarOpen(true)}
