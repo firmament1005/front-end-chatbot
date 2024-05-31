dc666fe4012c4faeb5f46b5a3666212b734c10c1
BlueSky428 <bluesky.engineer428@gmail.com>
2024-05-30 18:29:39 -0700

    Upgraded some featuers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Route from './routes';
 import './assets/css/style.css';
+import { ChatBotListProvider } from './Context/ChatListContext/ChatListContext';
 
 const App: React.FC = () => {
   return (
+    <ChatBotListProvider>
       <Route />
+    </ChatBotListProvider>
   )
 }
 
